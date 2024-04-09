194b80d7db747b31b3d32db6c69a83dacf95645e
space2024 <infocug123@gmail.com>
2024-04-09 11:37:20 +0530

    first commit

--- src/Components/Multi.jsx
+++ src/Components/Multi.jsx
@@ -395,7 +395,7 @@ const handleNext = async () => { // Make the function async
     e.preventDefault();
     try {
       console.log(formData)
-      const res = await axios.post(`http://localhost:3000/customer`, formData);
+      const res = await axios.post(`https://cust.spacetextiles.net/customer`, formData);
       console.log(res.data);
       setIsSuccess(true);
       setMBNo(res.data.mobileNo);
@@ -532,7 +532,7 @@ const handleNext = async () => { // Make the function async
 
   const handleVerify = async () => {
     try {
-      const response = await axios.get(`http://localhost:3000/verify_otp/${formData.otp}`);
+      const response = await axios.get(`https://cust.spacetextiles.net/verify_otp/${formData.otp}`);
     console.log(response)
       if (response.status === 200) {
         // OTP verification successful
